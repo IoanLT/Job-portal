b9a589610e791d0fbed181c8e4a9812667a7ef65
AyaDesigner <aya.berdyeva@gmail.com>
2020-11-24 20:28:08 +0100

    cleaned code, added function onclick search button

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 import './App.css';
 import React from 'react';
-// import Input from './components/Input';
-// import Button from './components/Button';
 import JobList from './components/JobList';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = {}
   }
+
   
-  getJobResults = () => {
-    console.log("function is called");
-    
-}
-  
-  render() { 
-    return ( 
+
+  render() {
+    return (
       <div>
         <h1>Hello Chucks</h1>
-        
-        {/* <Input /> */}
-        {/* <Button /> */}
-        <JobList />        
-    </div>);
+     
+        <JobList />
+      </div>);
   }
 }
 
--- src/components/JobList.js
+++ src/components/JobList.js
@@ -6,63 +6,48 @@ import Job from './Job';
 class JobList extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             jobsArray: []
         }
-        this.getJobs = this.getJobs.bind(this);
     }
 
-componentDidMount(){
-    this.getJobs();
-}
 
-// componentDidMount() {
-//     fetch('https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?page=1&search=code')
-//       .then(res => res.json())
-//       .then(data => console.log(data))
-//   }
 
-getJobs(){
-    Axios.get('https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?page=1&search=code')
-        .then(response => response.data)
-        .then(data => {
-            console.log(data);
-            this.setState({
-                jobsArray: data
+    getJobs = () => {
+        console.log("HEY")
+        Axios.get('https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?page=1&search=code')
+            .then(response => response.data)
+            .then(data => {
+                console.log(data);
+                this.setState({
+                    jobsArray: data
+                })
             })
-        })
-}
+    }
+ 
 
-
-getJobResults = () => {
-    console.log("function is called");
-    
-}
-
-
-    render() { 
-        return (  
+    render() {
+        return (
             <div>
                 <input type="text" placeholder="e.g Front-end development"></input>
-                <button onClick={this.getJobResults}>Search</button>
+                <button onClick={this.getJobs}>Search</button>
                 {
                     this.state.jobsArray.map((jobObject) => {
                         return (
-                            <Job 
-                                title = {jobObject.title} 
-                                type = {jobObject.type}
-                                location ={jobObject.location}
+                            <Job
+                                title={jobObject.title}
+                                type={jobObject.type}
+                                location={jobObject.location}
                                 company={jobObject.company}
                                 // key={index} 
                                 key={jobObject.id}
                             />
                         )
-                    } )
+                    })
                 }
             </div>
-            );
+        );
     }
 }
- 
+
 export default JobList;
-// https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?page=1&search=code
