6cd367feedc2f65359c6d577232f03db6bee19b7
AyaDesigner <aya.berdyeva@gmail.com>
2020-12-07 20:15:22 +0100

    added tabs to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,25 +1,96 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 import MyBoard from './MyBoard';
 import JobList from './JobList';
-import './Dashboard.css';
 
-export default class Dashboard extends Component {
-    render() {
-        return (
-            <div className="dashboard-container" id="dashboard">
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+        
+    },
+}));
+
+export default function Dashboard(props) {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static"
+                color="default">
+                <Tabs value={value}
+                    onChange={handleChange}
+                    aria-label="simple tabs example"
+                    variant="fullWidth"
+                    TabIndicatorProps={{ style: { background: "#3fb16a" } }}
+                    
+                    >
+                    <Tab label="Browse all" {...a11yProps(0)} />
+                    <Tab label="Saved" {...a11yProps(1)} />
+                    <Tab label="My CVs" {...a11yProps(2)} />
+                </Tabs>
+            </AppBar>
+            <TabPanel value={value} index={0}>
                 <JobList
-                    jobsArray={this.props.jobsArray}
-                    jobStatus={this.props.jobStatus}
-                    toggleJobInSavedJobs={this.props.toggleJobInSavedJobs}
+                    jobsArray={props.jobsArray}
+                    jobStatus={props.jobStatus}
+                    toggleJobInSavedJobs={props.toggleJobInSavedJobs}
                 />
-                {this.props.jobStatus && (
-                    <MyBoard
-                        savedJobsArray={this.props.savedJobsArray}
-                        jobStatus={this.props.jobStatus}
-                        toggleJobInSavedJobs={this.props.toggleJobInSavedJobs}
-                    />
-                )}
-            </div>
-        );
-    }
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                <MyBoard
+                    savedJobsArray={props.savedJobsArray}
+                    jobStatus={props.jobStatus}
+                    toggleJobInSavedJobs={props.toggleJobInSavedJobs}
+                />
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+                CV
+      </TabPanel>
+        </div>
+    );
 }
