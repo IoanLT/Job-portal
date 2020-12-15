ff5720853ba74a60cd779e3fa7810a4f3ccb9693
Viktoryia <kulinkovichva@gmail.com>
2020-12-15 14:21:10 +0300

    sign up button

--- src/components/Footer/SocialMediaLinks.js
+++ src/components/Footer/SocialMediaLinks.js
@@ -1,35 +1,63 @@
 import React from 'react';
 import './SocialMediaLinks.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
-  
+import {
+    faLinkedin,
+    faFacebook,
+    faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
+import { useAuth0 } from '@auth0/auth0-react';
+
 export default function SocialMediaLinks() {
+    const { loginWithRedirect, isAuthenticated } = useAuth0();
     return (
         <div className="social-media">
             <h2 className="follow--social">Follow us on:</h2>
-            <ul id="social-media-links">                  
+            <ul id="social-media-links">
                 <li>
-                    <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+                    <a
+                        href="https://www.facebook.com/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
                         <FontAwesomeIcon icon={faFacebook} />
                     </a>
                 </li>
                 <li>
-                    <a href="https://twitter.com/" target="_blank" rel="noreferrer">
+                    <a
+                        href="https://twitter.com/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
                         <FontAwesomeIcon icon={faTwitter} />
                     </a>
                 </li>
                 <li>
-                    <a href="https://linkedin.com/" target="_blank" rel="noreferrer">
+                    <a
+                        href="https://linkedin.com/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
                         <FontAwesomeIcon icon={faLinkedin} />
                     </a>
                 </li>
             </ul>
 
-            <section className="social--media--account">
-                <h4>Don't have an account ?</h4>
-                <button className="social--media--btn">Sign up now!</button>
-            </section>         
-            
+            {!isAuthenticated && (
+                <section className="social--media--account">
+                    <h4>Don't have an account ?</h4>
+                    <button
+                        onClick={() => {
+                            loginWithRedirect({
+                                screen_hint: 'signup',
+                            });
+                        }}
+                        className="social--media--btn"
+                    >
+                        Sign up now!
+                    </button>
+                </section>
+            )}
         </div>
-    )
+    );
 }
